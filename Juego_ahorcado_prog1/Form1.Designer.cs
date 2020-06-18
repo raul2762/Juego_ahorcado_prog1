63eb1fedf7e52dd13fab6a8483bf927ed144156c
Raul Guaroa Montan Rodriguez <raul.montan@inapa.gob.do>
2020-06-17 22:32:58 -0400

    Add project files.

---
+++ Juego_ahorcado_prog1/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Juego_ahorcado_prog1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.imgCuerpo = new System.Windows.Forms.PictureBox();
+			this.imgPieRight = new System.Windows.Forms.PictureBox();
+			this.imgPieLeft = new System.Windows.Forms.PictureBox();
+			this.imgBrazoLeft = new System.Windows.Forms.PictureBox();
+			this.imgBrazoRight = new System.Windows.Forms.PictureBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.btnContainer1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgCuerpo)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgPieRight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgPieLeft)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgBrazoLeft)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgBrazoRight)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0001s_0000_Horca_2_copy;
+			this.pictureBox1.Location = new System.Drawing.Point(58, 29);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(167, 365);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0000_Cabeza;
+			this.pictureBox2.Location = new System.Drawing.Point(187, 58);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(69, 67);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// imgCuerpo
+			// 
+			this.imgCuerpo.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0005_Cuerpo;
+			this.imgCuerpo.Location = new System.Drawing.Point(215, 80);
+			this.imgCuerpo.Name = "imgCuerpo";
+			this.imgCuerpo.Size = new System.Drawing.Size(14, 242);
+			this.imgCuerpo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgCuerpo.TabIndex = 3;
+			this.imgCuerpo.TabStop = false;
+			this.imgCuerpo.Visible = false;
+			// 
+			// imgPieRight
+			// 
+			this.imgPieRight.BackColor = System.Drawing.Color.Transparent;
+			this.imgPieRight.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0004_Pierna_izquierda;
+			this.imgPieRight.Location = new System.Drawing.Point(177, 305);
+			this.imgPieRight.Name = "imgPieRight";
+			this.imgPieRight.Size = new System.Drawing.Size(48, 72);
+			this.imgPieRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgPieRight.TabIndex = 4;
+			this.imgPieRight.TabStop = false;
+			this.imgPieRight.Visible = false;
+			// 
+			// imgPieLeft
+			// 
+			this.imgPieLeft.BackColor = System.Drawing.Color.Transparent;
+			this.imgPieLeft.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0003_Pierna_derecha;
+			this.imgPieLeft.Location = new System.Drawing.Point(219, 304);
+			this.imgPieLeft.Name = "imgPieLeft";
+			this.imgPieLeft.Size = new System.Drawing.Size(48, 72);
+			this.imgPieLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgPieLeft.TabIndex = 5;
+			this.imgPieLeft.TabStop = false;
+			this.imgPieLeft.Visible = false;
+			// 
+			// imgBrazoLeft
+			// 
+			this.imgBrazoLeft.Cursor = System.Windows.Forms.Cursors.Arrow;
+			this.imgBrazoLeft.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0001_Brazo_derecho;
+			this.imgBrazoLeft.Location = new System.Drawing.Point(219, 142);
+			this.imgBrazoLeft.Name = "imgBrazoLeft";
+			this.imgBrazoLeft.Size = new System.Drawing.Size(70, 68);
+			this.imgBrazoLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgBrazoLeft.TabIndex = 6;
+			this.imgBrazoLeft.TabStop = false;
+			this.imgBrazoLeft.Visible = false;
+			// 
+			// imgBrazoRight
+			// 
+			this.imgBrazoRight.Image = global::Juego_ahorcado_prog1.Properties.Resources.Ahorcado_0000s_0002_Brazo_izquierdo;
+			this.imgBrazoRight.Location = new System.Drawing.Point(155, 143);
+			this.imgBrazoRight.Name = "imgBrazoRight";
+			this.imgBrazoRight.Size = new System.Drawing.Size(70, 68);
+			this.imgBrazoRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.imgBrazoRight.TabIndex = 7;
+			this.imgBrazoRight.TabStop = false;
+			this.imgBrazoRight.Visible = false;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(458, 58);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(173, 21);
+			this.comboBox1.TabIndex = 16;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(586, 156);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(104, 23);
+			this.button1.TabIndex = 17;
+			this.button1.Text = "Agregar Boton";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// btnContainer1
+			// 
+			this.btnContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.btnContainer1.Location = new System.Drawing.Point(527, 185);
+			this.btnContainer1.Name = "btnContainer1";
+			this.btnContainer1.Size = new System.Drawing.Size(251, 137);
+			this.btnContainer1.TabIndex = 18;
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 300;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnContainer1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.imgCuerpo);
+			this.Controls.Add(this.imgPieRight);
+			this.Controls.Add(this.imgPieLeft);
+			this.Controls.Add(this.imgBrazoLeft);
+			this.Controls.Add(this.imgBrazoRight);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgCuerpo)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgPieRight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgPieLeft)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgBrazoLeft)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.imgBrazoRight)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox imgCuerpo;
+		private System.Windows.Forms.PictureBox imgPieRight;
+		private System.Windows.Forms.PictureBox imgPieLeft;
+		private System.Windows.Forms.PictureBox imgBrazoLeft;
+		private System.Windows.Forms.PictureBox imgBrazoRight;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.FlowLayoutPanel btnContainer1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ Juego_ahorcado_prog1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_ahorcado_prog1
+{
+	public partial class Form1 : Form
+	{
+		List<string> AnimalesList = new List<string>();
+		List<string> FrutasList = new List<string>();
+		List<string> MarcaCarrosList = new List<string>();
+		List<string> nombreBtn = new List<string>();
+		int contador = 0;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			#region Lista Letras
+			nombreBtn.Add("A");
+			nombreBtn.Add("B");
+			nombreBtn.Add("C");
+			nombreBtn.Add("D");
+			nombreBtn.Add("E");
+			nombreBtn.Add("F");
+			nombreBtn.Add("G");
+			nombreBtn.Add("H");
+			nombreBtn.Add("I");
+			nombreBtn.Add("J");
+			nombreBtn.Add("K");
+			nombreBtn.Add("L");
+			nombreBtn.Add("M");
+			nombreBtn.Add("N");
+			nombreBtn.Add("Ñ");
+			nombreBtn.Add("O");
+			nombreBtn.Add("P");
+			nombreBtn.Add("Q");
+			nombreBtn.Add("R");
+			nombreBtn.Add("S");
+			nombreBtn.Add("T");
+			nombreBtn.Add("U");
+			nombreBtn.Add("V");
+			nombreBtn.Add("W");
+			nombreBtn.Add("X");
+			nombreBtn.Add("Y");
+			nombreBtn.Add("Z");
+			#endregion
+
+			#region Lista Animales
+			AnimalesList.Add("leon");
+			AnimalesList.Add("cangrejo");
+			AnimalesList.Add("canguro");
+			AnimalesList.Add("bisonte");
+			AnimalesList.Add("elefante");
+			AnimalesList.Add("foca");
+			AnimalesList.Add("jabali");
+			AnimalesList.Add("loro");
+			AnimalesList.Add("ciervo");
+			AnimalesList.Add("bufalo");
+			#endregion
+
+			#region Lista Frutas
+			FrutasList.Add("arandano");
+			FrutasList.Add("cereza");
+			FrutasList.Add("ciruela");
+			FrutasList.Add("fresa");
+			FrutasList.Add("mandarina");
+			FrutasList.Add("melon");
+			FrutasList.Add("kiwi");
+			FrutasList.Add("coco");
+			FrutasList.Add("manzana");
+			FrutasList.Add("uva");
+			#endregion
+
+			#region Lista Marca de Carros
+			MarcaCarrosList.Add("skoda");
+			MarcaCarrosList.Add("seat");
+			MarcaCarrosList.Add("bently");
+			MarcaCarrosList.Add("citroen");
+			MarcaCarrosList.Add("renault");
+			MarcaCarrosList.Add("suzuki");
+			MarcaCarrosList.Add("lotus");
+			MarcaCarrosList.Add("lexus");
+			MarcaCarrosList.Add("tesla");
+			MarcaCarrosList.Add("volvo");
+			#endregion
+
+			comboBox1.Items.Add("Seleccione una categoria");
+			comboBox1.Items.Add("Animales");
+			comboBox1.Items.Add("Frutas");
+			comboBox1.Items.Add("Marca de Carros");
+			comboBox1.SelectedIndex = 0;
+			timer1.Start();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Has hecho click en el Boton");
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void handlerComun_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(((Button)sender).Text);
+			btnContainer1.Controls.Remove(((Button)sender));
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			if (contador < nombreBtn.Count)
+			{
+				Button button = new Button();
+
+				button.Height = 23;
+				button.Width = 29;
+				button.Name = nombreBtn[contador];
+				button.Text = nombreBtn[contador];
+				button.Click += new EventHandler(handlerComun_Click);
+				btnContainer1.Controls.Add(button);
+				contador++;
+			}
+			else
+			{
+				timer1.Stop();
+			}
+			
+		}
+	}
+}
